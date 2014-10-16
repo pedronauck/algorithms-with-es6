35ab0a0284112ef563fb099e00dd9066a8bac6f3
Pedro Nauck <pedronauck@gmail.com>
2014-10-16 14:15:40 -0300

    bugfix: delete old fatorial script

--- dist/math/fatorial.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var fatorial = function(num) {
-    var result = 1;
-    while (--num) result *= num + 1;
-    return result;
-  };
-
-  module.exports = fatorial;
-})();
--- src/math/fatorial.js
+++
@@ -1,7 +0,0 @@
-let fatorial = (num) => {
-  let result = 1;
-  while (--num) result *= num + 1;
-  return result;
-};
-
-module.exports = fatorial;
