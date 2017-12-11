4d5124175ded2ccfb56ca6a86e41166ceabbdbee
Kenny <kennyudovic@gmail.com>
2017-12-11 11:33:39 -0500

    Fixed remove-from-array. Multiple matches in a row would produce bad results.

--- src/array/remove-from-array.js
+++ src/array/remove-from-array.js
@@ -1,5 +1,9 @@
 let removeFromArr = (arr, val) => {
-  arr.forEach((num, i) => { if (num === val) arr.splice(i, 1) });
+  for(let i = arr.length - 1; i >= 0; i--){
+    if(arr[i] === val) 
+      arr.splice(i, 1);
+  }
+  return arr;
 };
 
 module.exports = removeFromArr;
